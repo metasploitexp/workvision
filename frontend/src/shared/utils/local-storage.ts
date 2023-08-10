832b2820be91587521fbf4a07b609a35cb969ec1
Alexandra55557 <Alexandra55557@example.com>
2023-08-10 16:11:36 +0300

    упростила функции после код ревью

--- frontend/src/shared/utils/local-storage.ts
+++ frontend/src/shared/utils/local-storage.ts
@@ -1,15 +1,8 @@
 
-export function getDataFromLocalStorage(variableName: string, isJSON: boolean = false) {
-    const data = localStorage.getItem(variableName);
-    if (isJSON && data) {
-        return JSON.parse(data);
-    }
-    return data;
+export function get(name: string, isJSON: boolean = false) {
+    return isJSON ? JSON.parse(localStorage.getItem(name) || '') : localStorage.getItem(name);
 }
 
-export function setDataToLocalStorage(variableName: string, value: any, isJSON: boolean = false) {
-    if(isJSON) {
-        value = JSON.stringify(value);
-    }
-    localStorage.setItem(variableName, value);
+export function set(name: string, value: any, isJSON: boolean = false) {
+    localStorage.setItem(name, isJSON ? JSON.stringify(value) : value);
 }
