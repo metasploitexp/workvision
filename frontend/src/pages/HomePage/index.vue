58f1eb53563dc354e5dd4dbda7133db4bdd678ef
metasploit <metasploit>
2023-08-25 15:43:25 +0300

    #7 fix

--- frontend/src/pages/HomePage/index.vue
+++ frontend/src/pages/HomePage/index.vue
@@ -12,7 +12,5 @@ export default {
 </script>
 
 <style lang="scss">
-    .home-page {
-        // @include default-page;
-    }
+    
 </style>
--- frontend/src/pages/LoginPage/index.vue
+++ frontend/src/pages/LoginPage/index.vue
@@ -16,7 +16,5 @@ export default {
 </script>
 
 <style lang="scss">
-    .login-page {
-        @include default-page;
-    }
+    
 </style>
