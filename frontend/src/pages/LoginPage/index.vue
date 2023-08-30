89a950c12e1411dfa377525aca273298dabdfc70
Сделал таск # 8 <vip.rabotnik1996@mail.ru>
2023-08-30 23:45:45 +0300

    Сделал таск # 8

--- frontend/src/pages/LoginPage/index.vue
+++ frontend/src/pages/LoginPage/index.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class="login-page">
-        <LoginForm />
-    </div>
+	<div class="login-page">
+		<LoginForm />
+	</div>
 </template>
 
 <script>
-import { LoginForm } from '@/features/LoginForm';
+	import { LoginForm } from "@/features/LoginForm";
 
-export default {
-    name: 'loginPage',
-    components: {
-        LoginForm,
-    }
-}
+	export default {
+		name: "loginPage",
+		components: {
+			LoginForm,
+		},
+	};
 </script>
 
-<style lang="scss">
-    
-</style>
+<style lang="scss"></style>
--- frontend/src/shared/utils/tine.ts
+++ frontend/src/shared/utils/tine.ts
@@ -1,22 +1,22 @@
 import moment from "moment";
-/* Функция для сравнения двух дат */
-export function compareDates(date1: Date, date2: Date): number {
+
+/* функция для сравнения двух дат  */
+export function compareDates(date1: Date, date2: Date): boolean {
 	const momentDate1 = moment(date1);
 	const momentDate2 = moment(date2);
-	if (momentDate1.isBefore(momentDate2)) {
-		return -1;
-	} else if (momentDate1.isAfter(momentDate2)) {
-		return 1;
-	} else {
-		return 0;
-	}
+	return momentDate1.isBefore(momentDate2);
 }
-// Функция для преобразования даты в формат "дд-мм-гггг"
-/* export function formatDate(date: Date): string {
-	return moment(date).format("DD-MM-YYYY");
-} */
+/* Функцию для определения какая дата идет раньше */
+export function isDateBefore(date1: Date, date2: Date): boolean {
+	return moment(date1).isBefore(date2);
+}
 
-// Функция для преобразования даты в формат "дд-мес-гггг"
+/* Функция для привидения даты к типу дд-мм-гггг (пример 22-08-2023) */
 export function formatDate(date: Date): string {
+	return moment(date).format("DD-MM-YYYY");
+}
+
+/* Функция для привидения даты к типу дд мес.гггг (пример 22 авг. 2023) */
+export function formatDateM(date: Date): string {
 	return moment(date).format("DD-MMM-YYYY");
 }
