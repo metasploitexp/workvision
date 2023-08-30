c963e4d868ca916a236e3835e8ef58b0b38a9532
залил или нет <vip.rabotnik1996@mail.ru>
2023-08-30 15:48:10 +0300

    здорова

--- frontend/src/features/LoginForm/index.vue
+++ frontend/src/features/LoginForm/index.vue
@@ -1,64 +1,68 @@
 <template>
-    <div class="login-form">
-        <WrapForm :title="$t('login.title')">
-            <div class="login-form__content">
-                <div class="login-form__content-mail">
-                    <div class="login-form__content-mail__label">
-                        {{ $t('login.mail') }}
-                    </div>
-                    <div class="login-form__content-mail__input">
-                        <TextInput type="mail" />
-                    </div>
-                </div>
-                <div class="login-form__content-password">
-                    <div class="login-form__content-password__label">
-                        {{ $t('login.password') }}
-                    </div>
-                    <div class="login-form__content-password__input">
-                        <TextInput type="password" />
-                    </div>
-                </div>
-                <div class="login-form__content-button">
-                    <Btn :text="$t('login.button')" />
-                </div>
-            </div>
-        </WrapForm>
-    </div>
+	<div class="login-form">
+		<WrapForm :title="$t('login.title')">
+			<div class="login-form__content">
+				<div class="login-form__content-mail">
+					<div class="login-form__content-mail__label">
+						{{ $t("login.mail") }}
+					</div>
+					<div class="login-form__content-mail__input">
+						<TextInput type="mail" />
+					</div>
+				</div>
+				<div class="login-form__content-password">
+					<div class="login-form__content-password__label">
+						{{ $t("login.password") }}
+					</div>
+					<div class="login-form__content-password__input">
+						<TextInput type="password" />
+					</div>
+				</div>
+				<div class="login-form__content-button">
+					<Btn :text="$t('login.button')" />
+				</div>
+			</div>
+		</WrapForm>
+	</div>
 </template>
 
 <script>
-import WrapForm from '@/shared/ui/WrapForm.vue';
-import TextInput from '@/shared/ui/TextInput.vue';
-import Btn from '@/shared/ui/Btn.vue';
+	import WrapForm from "@/shared/ui/WrapForm.vue";
+	import TextInput from "@/shared/ui/TextInput.vue";
+	import Btn from "@/shared/ui/Btn.vue";
+	/* 	import { formatDate } from "@/shared/utils/tine.ts"; */
 
-export default {
-    name: 'loginForm',
-    components: {
-        WrapForm,
-        TextInput,
-        Btn,
-    }
-}
+	export default {
+		name: "loginForm",
+		components: {
+			WrapForm,
+			TextInput,
+			Btn,
+		},
+	};
+	/* 	const date = new Date("2022-05-01");
+	const formattedDate = formatDate(date);
+	console.log(formattedDate); */
 </script>
 
 <style lang="scss">
-    .login-form {
-        @include absolute-centered;
-        
-        &__content {
-            @include default-flex(column, 20px);
+	.login-form {
+		@include absolute-centered;
 
-            &-mail, &-password {
-                @include default-flex(column, 10px);;
-                font-size: 18px;
-            }
+		&__content {
+			@include default-flex(column, 20px);
 
-            &-button {
-                margin: 0 auto;
-                width: fit-content;
-                font-size: 18px;
+			&-mail,
+			&-password {
+				@include default-flex(column, 10px);
+				font-size: 18px;
+			}
 
-            }
-        }
-    }
+			&-button {
+				margin: 0 auto;
+				width: fit-content;
+				font-size: 18px;
+			}
+		}
+	}
 </style>
