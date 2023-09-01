99d1f537323fb8ba297a096b57db1328b14df354
Сделал таск # 8 <vip.rabotnik1996@mail.ru>
2023-09-01 08:52:09 +0300

    Доработал функции по выводу даты по задаче # 8

--- frontend/src/pages/LoginPage/index.vue
+++ frontend/src/pages/LoginPage/index.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="login-page">
-        <LoginForm />
-    </div>
+	<div class="login-page">
+		<LoginForm />
+	</div>
 </template>
 
 <script>
-import { LoginForm } from '@/features/LoginForm';
+	import { LoginForm } from "@/features/LoginForm";
+	export default {
+		name: "loginPage",
+		components: {
+			LoginForm,
+		},
+	};
+	import { formatDateM } from "@/shared/utils/date.ts";
+	const date = "22 мая 1996";
+	const formattedDateM = formatDateM(date);
 
-export default {
-    name: 'loginPage',
-    components: {
-        LoginForm,
-    }
-}
+	console.log(formattedDateM);
 </script>
 
-<style lang="scss">
-    
-</style>
+<style lang="scss"></style>
--- frontend/src/shared/utils/date.ts
+++ frontend/src/shared/utils/date.ts
@@ -1,4 +1,5 @@
 import moment from "moment";
+import "moment/locale/ru";
 /* функция для проверки какая дата идёт по середине  */
 
 export function isDateBetween(
@@ -17,11 +18,13 @@ export function isDateBefore(date1: Date, date2: Date): boolean {
 }
 
 /* Функция для привидения даты к типу дд-мм-гггг (пример 22-08-2023) */
-export function formatDate(date: Date): string {
-	return moment(date).format("DD-MM-YYYY");
+export function formatDate(dateString: string): string {
+	const date = moment(dateString, "DD MM YYYY");
+	return date.format("DD MM YYYY");
 }
 
 /* Функция для привидения даты к типу дд мес.гггг (пример 22 авг. 2023) */
-export function formatDateM(date: Date): string {
-	return moment(date).format("DD-MMM-YYYY");
+export function formatDateM(dateString: string): string {
+	const date = moment(dateString, "DD MMMM YYYY");
+	return date.format("DD MMMM YYYY");
 }
