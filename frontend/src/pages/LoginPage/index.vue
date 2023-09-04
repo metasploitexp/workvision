a6ba951405c22c3b6a1d5976529997181ccba0da
Сделал таск # 8 <vip.rabotnik1996@mail.ru>
2023-09-04 23:47:51 +0300

    переделал функции по pullrequest

--- frontend/src/pages/LoginPage/index.vue
+++ frontend/src/pages/LoginPage/index.vue
@@ -1,21 +1,18 @@
 <template>
-	<div class="login-page">
-		<LoginForm />
-	</div>
+    <div class="login-page">
+        <LoginForm />
+    </div>
 </template>
-
 <script>
 import { LoginForm } from "@/features/LoginForm";
-
 export default {
-	name: "loginPage",
-	components: {
-		LoginForm,
-	},
+    name: "loginPage",
+    components: {
+        LoginForm,
+    },
 };
 </script>
-
 <style lang="scss">
 
 </style>
-	    	
+            
--- frontend/src/shared/utils/date.ts
+++ frontend/src/shared/utils/date.ts
@@ -2,30 +2,18 @@ import "moment/locale/ru";
 import moment from "moment";
 
 /* функция для проверки какая дата идёт по середине  */
+export function isDateBetween(date: any, startDate: any, endDate: any) :boolean {
+	return moment(date).isBetween('momentStarDate', 'momentEndDate', undefined, "[]")}
 
-export function isDateBetween(
-date: any,	
-startDate: any,
-endDate: any
-): boolean {
-	const momentDate = moment(date);
-	const momentStarDate = moment(startDate);
-	const momentEndDate = moment(endDate);
-	return momentDate.isBetween(momentStarDate, momentEndDate, undefined, "[]");
-}
 /* Функцию для определения какая дата идет раньше */
-
 export function isDateBefore(date1: any, date2: any) {
-	return moment(date1).isBefore(date2);
-}
+	return moment(date1).isBefore(date2)}
 
 /* Функция для привидения даты к типу дд-мм-гггг (пример 22-08-2023)*/
-
+const DATE_FORMATS = ['DD MMMM YYYY', 'DD MM YYYY']
 export function formatDate(date: any): string {
-	return moment(date, ["DD MMMM YYYY", "DD MM YYYY"]).format("DD MM YYYY");
-}
+	return moment(date, DATE_FORMATS).format("DD MM YYYY")}
 
 /* Функция для привидения даты к типу дд мес.гггг (пример 22 авг. 2023) */
 export function formatDateM(date: any): string {
-	return moment(date, ["DD MMMM YYYY", "DD MM YYYY"]).format("DD MMM YYYY");
-}
+	return moment(date, DATE_FORMATS ).format("DD MMM YYYY")}
