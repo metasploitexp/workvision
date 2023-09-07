294c2e5a10c52deacd9c8a06e35527f4204f30a6
Сделал таск # 8 <vip.rabotnik1996@mail.ru>
2023-09-08 00:21:25 +0300

    Сверстал создание новой карточки. Задача # 10

--- frontend/src/pages/BoardsPage/index.vue
+++ frontend/src/pages/BoardsPage/index.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="boards-page">
         <DesktopPanel />
+        <CreateBoard />
     </div>
 </template>
 
 <script>
 import { DesktopPanel } from '@/widgets/DesktopPanel';
+import { CreateBoard } from '@/features/CreateBoard';
 
 export default {
     name: 'boardsPage',
     components: {
         DesktopPanel,
+        CreateBoard
     }
 }
 </script>
