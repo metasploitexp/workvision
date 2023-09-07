191cbc0795b6214bac8d9cd38641b523fb3c4ba6
Сделал таск # 8 <vip.rabotnik1996@mail.ru>
2023-09-07 16:05:58 +0300

    Правки по задаче # 8

--- frontend/src/shared/utils/date.ts
+++ frontend/src/shared/utils/date.ts
@@ -3,16 +3,16 @@ import moment from 'moment';
 const DATE_FORMATS = ['DD MMMM YYYY', 'DD MM YYYY'];
 
 /* функция для проверки какая дата идёт по середине  */
-export function isDateBetween(date:any, startDate: any, endDate: any) :boolean {
+export function isDateBetween(date: any, startDate: any, endDate: any) :boolean {
     return moment(date).isBetween(moment(startDate), moment(endDate))};
 
 /* Функцию для определения какая дата идет раньше */
 export function isDateBefore(date1: any, date2: any) {
-    return moment(date2).isBefore(date2)};
+    return moment(date1).isBefore(date2)};
 
 /* Функция для привидения даты к типу дд-мм-гггг (пример 22-08-2023)*/
 export function formatDate(date: any): string {
-    return moment(date, DATE_FORMATS).format('DD MM YYYY')};
+    return moment(date, DATE_FORMATS).format('DD-MM-YYYY')};
 
 /* Функция для привидения даты к типу дд мес.гггг (пример 22 авг. 2023) */
 export function formatDateM(date: any): string {
