d01b025561c24d3d7686944483a364b3f9ffb4eb
smihlenyu <vip.rabotnik1996@mail.ru>
2024-01-26 12:38:02 +0300

    #20 add icon

---
+++ frontend/src/shared/ui/Loader.vue
@@ -0,0 +1,42 @@
+<template>
+
+<div class="loader">
+    <div> 
+        <img src="@/shared/assets/image/Loader.png" alt="dasd">
+    </div>
+</div>
+
+</template>
+
+<script>
+
+
+export default {
+    name:'loader',
+}
+
+</script>
+
+<style lang="scss">
+
+.loader {
+
+    & img {
+        width: 35px;
+        height: 35px;
+        animation: rotate 3s linear infinite;
+        margin-top: 3px;
+        margin-right: 3px;
+    }
+
+    @keyframes rotate {
+        from {
+        transform: rotate(0deg);
+        }
+        to {
+        transform: rotate(360deg);
+        }
+    }
+}
+
+</style>
